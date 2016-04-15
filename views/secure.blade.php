1db59fd70ea62b95ca8c446e3180b54053b7a261
Ricardo Fidelis <ricardothehot@gmail.com>
2016-04-15 12:49:56 -0300

    Versão Inicial

---
+++ views/secure.blade.php
@@ -0,0 +1,36 @@
+@layout('Master')
+
+@section('title')Imoveis | Autentica&ccedil;&atilde;o Padr&atilde;o @endsection
+
+@section('content')
+<div class="container">
+
+	@if (isset($feedback))
+		<div class="alert alert-error">
+			<button type="button" class="close" data-dismiss="alert">×</button>
+			{{ htmlentities($feedback) }}
+		</div>
+	@endif
+	
+	<!-- #### this view/tempalate is used for multiple pages.  the controller sets the 'page' variable to display differnet content ####  -->
+	
+	@if ($page == 'login')
+	
+		<form class="well" method="post" action="login">
+		<fieldset>
+			<legend>Informe seu usu&aacute;rio e senha</legend>
+				<div class="control-group">
+				<input id="username" name="username" type="text" placeholder="Usu&aacute;rio..." />
+				</div>
+				<div class="control-group">
+				<input id="password" name="password" type="password" placeholder="Senha..." />
+				</div>
+				<div class="control-group">
+				<button type="submit" class="btn btn-primary">Login</button>
+				</div>
+			</fieldset>
+		</form>
+	@endif
+
+</div> <!-- /container -->
+@endsection
